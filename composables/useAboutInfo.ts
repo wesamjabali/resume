1bc89677126e0c2c05908addc704d8dfd2cc7fa7
Wesam Jabali <wesamjabali@icloud.com>
2023-03-19 13:24:07 -0500

    update about

--- composables/useAboutInfo.ts
+++ composables/useAboutInfo.ts
@@ -3,7 +3,7 @@ const useAboutInfo = () => {
     title: "About",
     description: `Hey there, I'm Wesam. I'm a fullstack developer with a primary focus on frontend for my main project. \
 I enjoy building responsive and user-friendly applications that cater to the diverse needs of clients. \
-In my free time, I enjoy exploring and experimenting with new technologies, constantly learning and improving my skills.`,
+In my free time, I enjoy exploring and experimenting with new technologies, further learning and improving my skills.`,
   };
 };
 
