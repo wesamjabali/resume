bc3c430e03ad0bd287c2e059453d8669ac64bcec
Wesam Jabali <wesamjabali@icloud.com>
2023-06-19 17:02:00 -0500

    remove margin from about

--- composables/useAboutInfo.ts
+++ composables/useAboutInfo.ts
@@ -1,7 +1,7 @@
 const useAboutInfo = () => {
   return {
     title: "About",
-    description: `<p class="paragraph">Hey there, I'm Wesam. I'm an experienced fullstack developer creating immersive user interfaces and scalable applications. \
+    description: `<p style="display: block;">Hey there, I'm Wesam. I'm an experienced fullstack developer creating immersive user interfaces and scalable applications. \
 I'm based in Chicago, but currently living in Düsseldorf, Germany. </p>\
 <p>As a Vue expert, I focus on optimizing performance and enhancing user interactions with my team. \
 Additionally, I excel in team leadership and mentoring fellow developers.  \
